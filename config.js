bbed085b0dfd3589c3c9cc4ed047de4ed5c7d3a2
Frei-vms <federico89py@gmail.com>
2026-09-19 11:58:36 -0300

    Correct contact object format in config.js
    Fix contact object structure and remove phone number.

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ const STORE_CONFIG = {
   },
 
   /* ---- Contacto (déjalo vacío "" para ocultar un dato) ---- */
-  contact: {
-    phone: "595984183711",                      
+  contact: 595984183711{
+    phone: "",                      
     email: "",                      // Agregar cuando se disponga de un correo
     address: "",                    // Agregar la dirección del local si se desea mostrarla
     hours: "Atención de 07:00 a 19:00"
